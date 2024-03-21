28d1dea634711641a4ae03cfd9a8ef30193601f0
Emre Gül <144537902+emre-mr246@users.noreply.github.com>
2024-03-21 22:24:46 +0300

    the appearance of the repository has been improved

---
+++ get_next_line/main.c
@@ -0,0 +1,20 @@
+#include "get_next_line.h"
+
+/*int	main(void)
+{
+	char	*line;
+	int		fd1;
+
+	fd1 = open("tests/test.txt", O_RDONLY);
+
+    line = get_next_line(fd1);
+    printf("%s", line);
+    free(line);
+
+    line = get_next_line(fd1);
+    printf("%s", line);
+    free(line);
+
+	close(fd1);
+	return (0);
+}*/
---
+++ get_next_line_bonus/main.c
@@ -0,0 +1,27 @@
+#include "get_next_line_bonus.h"
+
+/*int	main(void)
+{
+	char	*line;
+	int		fd1;
+	int		fd2;
+
+	fd1 = open("tests/test.txt", O_RDONLY);
+	fd2 = open("tests/test2.txt", O_RDONLY);
+
+    line = get_next_line(fd1);
+    printf("%s, line);
+    free(line);
+
+    line = get_next_line(fd2);
+    printf("%s, line);
+    free(line);
+
+    line = get_next_line(fd1);
+    printf("%s", line);
+    free(line);
+
+	close(fd1);
+	close(fd2);
+	return (0);
+}*/
